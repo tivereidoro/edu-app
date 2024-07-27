afd9e691cfa4d0bde2553665223cae0e7f79c9e1
tivereidoro <tivereidoro@yahoo.com>
2024-07-27 01:16:22 +0100

    Navbar coming up

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './navbar.css';
-import logo from '../../../public/icon.png';
+import logo from '../../assets/images/icon.png';
 
 export default function Navbar() {
     return (
-        <nav>
-            <img src={logo} alt="homepage_logo" />
+        <nav className='def_container'>
+            <img className='home_logo' src={logo} alt="homepage_logo" />
 
             <ul>
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Campus</li>
-                <li>About us</li>
-                <li>Contact us</li>
+                <li><a>Home</a></li>
+                <li><a>Programs</a></li>
+                <li><a>Campus</a></li>
+                <li><a>About us</a></li>
+                <li><button className='main_btn'><a>Contact us</a></button></li>
             </ul>
         </nav>
     )
